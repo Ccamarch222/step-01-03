8c2b7a3e5c85411a7a954c70bf2952e0e5d5ce3a
MaryamAkberAli <ccamarch202207@gmail.com>
2024-08-18 01:41:27 +0500

    this repo learn how to navigate in next.js

---
+++ src/app/homePage/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+    return(
+        <div className="font-serif font-bold text-center">
+        
+        <p>This is home page</p>
+        </div>
+    )
+}
---
+++ src/app/name/page.tsx
@@ -0,0 +1,7 @@
+export default function name(){
+    return (
+        <div className="font-serif font-bold text-center">
+            <h1>Namepage</h1>
+        </div>
+    )
+}
